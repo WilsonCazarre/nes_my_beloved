3c23a5529f56d94d3995694e7c312e8ca1f99e87
WilsonCazarre <wcs0486@gmail.com>
2023-11-18 13:26:03 -0300

    file watchers

--- src/main.s
+++ src/main.s
@@ -62,9 +62,7 @@ POOL_CONTROLLER = $30
 
   jsr LoadPalettes
 
-  ; jsr LoadNametables
-  LoadAddressToVram LoadNametables::TILES_BUFFER, CTRL_TILES, #$06
-  LoadStringToVram $2081, hello
+  jsr LoadNametables
   
   InitPPU
 
@@ -92,12 +90,6 @@ POOL_CONTROLLER = $30
   jsr PlayBeep
 @return:
   jmp @GameLoop
-
-CTRL_TILES:
-  .byte $F0, $F1, $F2, $F3, $F4, $F5
-
-hello:
-  .byte "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum.", $0
 
 .endproc
 
