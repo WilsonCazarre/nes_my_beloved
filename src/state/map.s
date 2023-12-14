43b1f9acf7fa206152390bac498495a23695a2b6
IgorFFerraz <igor.ferreira.ferraz@gmail.com>
2023-12-14 09:58:56 -0300

    new tiles

--- src/state/map.s
+++ src/state/map.s
@@ -27,17 +27,56 @@
     lda #CONVEYOR_BELT_TILE+$11
     sta PPU_DATA
 
-    SetVramAddress CONVEYOR_BELT_LOCATION+$20
+    SetVramAddress CONVEYOR_BELT_LOCATION+(PPU_LINE_LENGTH*2)
     lda #CONVEYOR_BELT_TILE+$10
     sta PPU_DATA
     lda #CONVEYOR_BELT_TILE+$11
     sta PPU_DATA
 
-    SetVramAddress CONVEYOR_BELT_LOCATION+$20+PPU_LINE_LENGTH
+    SetVramAddress CONVEYOR_BELT_LOCATION+(PPU_LINE_LENGTH*3)
     lda #CONVEYOR_BELT_TILE+$10
     sta PPU_DATA
     lda #CONVEYOR_BELT_TILE+$11
     sta PPU_DATA
+    
+    ; FLOOR SOUTH
+    SetVramAddress FLOOR_EMPTY_SOUTH
+    lda #FLOOR_EMPTY_TILE
+    ldx #0
+
+  @floor_loop:
+    sta PPU_DATA
+    inx
+    cpx #FLOOR_EMPTY_LENGTH
+    bne @floor_loop
+    
+    ; FLOOR TOP LEFT
+    ; SetVramAddress FLOOR_EMPTY_TOPLEFT
+    ; lda #FLOOR_EMPTY_TILE
+    ; ldx #$0 ; Row
+    ; @newRow:
+    ; ldy #$0 
+
+    ; @topl_loop:
+    ; sta PPU_DATA
+    ; inx
+    ; cpx #
+    ; bne @topl_loop
+    ; iny
+    ; cpy #
+    ; SetVramAddress FLOOR_EMPTY_TOPLEFT+PPU_LINE_LENGTH
+    ; bne @newRow
+
+    ; ; FLOOR TOP RIGHT
+    ; SetVramAddress FLOOR_EMPTY_SOUTH
+    ; lda #FLOOR_EMPTY_TILE
+    ; ldx #0
+
+    ; @floor_loop:
+    ; sta PPU_DATA
+    ; inx
+    ; cpx #FLOOR_EMPTY_LENGTH
+    ; bne @floor_loop
 
     rts
   .endproc
@@ -49,8 +88,14 @@
   .proc frameUpdate
     rts
   .endproc
+
   CABINET_TILE = $01
   CABINET_LOCATION = $20b7
   CONVEYOR_BELT_TILE = $98
   CONVEYOR_BELT_LOCATION = $202F
+  FLOOR_EMPTY_TILE = $FC
+  FLOOR_EMPTY_SOUTH = $2300
+  FLOOR_EMPTY_TOPLEFT = $2000
+  FLOOR_EMPTY_TOPRIGHT = $2017
+  FLOOR_EMPTY_LENGTH = $BF
 .endscope
